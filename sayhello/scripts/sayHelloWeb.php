59cd46b96388b2f4e5ac72ceefa81ef491243a67
donglihan-f21 <donglihan@hotmail.com>
2015-08-16 00:05:33 +0800

    15-8-16 00-05

---
+++ sayhello/scripts/sayHelloWeb.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <link href="../css/phpMM.css" rel="stylesheet" type="text/css" />
+ </head>
+
+ <body>
+  <div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+  <div id="example">Example 2-1</div>
+
+  <div id="content">
+    <h1>Welcome!  <?php echo $_REQUEST['name'];?>! </br>You are <?php echo $_REQUEST['year'];?> years old.</h1>
+    <p>Great to meet you.</p>
+  </div>
+
+  <div id="footer"></div>
+ </body>
+</html>
